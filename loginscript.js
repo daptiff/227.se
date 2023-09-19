a22809d1931186b675303a161be53b987897cf06
selflessness0 <143733152+selflessness0@users.noreply.github.com>
2023-09-19 17:16:17 +0200

    19 09 23 (1)

--- loginscript.js
+++ loginscript.js
@@ -2,8 +2,7 @@ function attemptLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const loginResult = document.getElementById('login-result');
-
-    // Simulate a simple login
+    
     if (username === 'admin' && password === 'admin') {
         loginResult.innerHTML = '';
         setTimeout(() => {
