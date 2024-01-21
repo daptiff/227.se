a2c5210f43cdb34906aecf9fd7c60865b8bbab71
selflessness0 <143733152+selflessness0@users.noreply.github.com>
2024-01-21 16:43:08 +0100

    rep

---
+++ res/content.js
@@ -0,0 +1,7 @@
+//const titleText = 'selflessness.online';
+const scrollText = 'fj/c cds 4 sale @@@ salemrecords.bandcamp.com !!!';
+
+document.addEventListener('DOMContentLoaded', function () {
+    //document.querySelector('.title-wrap a').innerText = titleText;
+    document.querySelector('.scrollText').innerText = scrollText;
+});
